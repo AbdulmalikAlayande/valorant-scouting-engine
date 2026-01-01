0bd4c47d3748c394e555884157c8ba4cb32c87b2
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-01 21:51:15 +0100

    feat: implement win rate calculation and logging for team game statistics

--- transforms/team_tendencies.py
+++ transforms/team_tendencies.py
@@ -1,9 +1,24 @@
+import pandas as pd
+from ingest.fetch_stats import ingest_team_statistics, ingest_team_game_statistics
+from ingest.fetch_series import ingest_team_recent_series, ingest_series_by_time_range
+from config.globalutilitylogger import get_logger
+
+_logger = get_logger(__name__)
+
+
 def calculate_win_rates(team_game_stats):
     """
     What: Computes overall win rate and map-specific win rates.
     Why: Vital for the "Map Performance" section of the report.
     """
-    pass
+    if team_game_stats is None:
+        _logger.info("No team game stats provided.")
+        return
+
+    dataframe = pd.json_normalize(team_game_stats)
+    print(dataframe.head(10))
+    _logger.info(dataframe.head(10))
+
 
 def analyze_map_veto_strategy(map_status):
     """
@@ -18,3 +33,8 @@ def detect_strategic_trends(game_strategies):
     Why: Helps coaches understand if the opponent is currently on an upswing or struggling. And Supports the "Recent Trends" section and offers insights into evolving team tactics.
     """
     pass
+
+
+if __name__ == '__main__':
+    statistics = ingest_team_game_statistics(team_id="1079", time_window="LAST_YEAR")
+    calculate_win_rates(statistics)
