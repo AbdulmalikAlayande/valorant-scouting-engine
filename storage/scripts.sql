b8019081cd5d53be59310bac118bc4280223b92a
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-08 10:49:26 +0100

    refactor: improve logging clarity, remove redundant `continue` in error handling, and add new test `create_report_request` calls

--- storage/scripts.sql
+++ storage/scripts.sql
@@ -1,5 +1,5 @@
--- delete from scouting_reports;
--- delete from report_requests;
+delete from scouting_reports;
+delete from report_requests;
 --
 -- alter table report_requests
 --     drop column if exists user_prompt;
@@ -10,11 +10,11 @@
 -- alter table report_requests
 --     add user_prompt text default ''::text not null;
 
-alter table report_requests
-    drop column if exists team_id;
-alter table report_requests
-    drop column if exists time_window;
-alter table report_requests
-    drop column if exists team_fk_id;
-alter table report_requests
-    drop column if exists team_name
+-- alter table report_requests
+--     drop column if exists team_id;
+-- alter table report_requests
+--     drop column if exists time_window;
+-- alter table report_requests
+--     drop column if exists team_fk_id;
+-- alter table report_requests
+--     drop column if exists team_name
