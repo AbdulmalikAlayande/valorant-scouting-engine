5f6d3f89ea1c48d986df43611f9f9114898cfc65
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-08 09:55:47 +0100

    feat: add `user_prompt` column to `report_requests` and create index for pending status queries

---
+++ storage/scripts.sql
@@ -0,0 +1,20 @@
+-- delete from scouting_reports;
+-- delete from report_requests;
+--
+-- alter table report_requests
+--     drop column if exists user_prompt;
+-- alter table report_requests
+--     drop column if exists status;
+-- alter table report_requests
+--     add status varchar(50) default 'pending'::character varying;
+-- alter table report_requests
+--     add user_prompt text default ''::text not null;
+
+alter table report_requests
+    drop column if exists team_id;
+alter table report_requests
+    drop column if exists time_window;
+alter table report_requests
+    drop column if exists team_fk_id;
+alter table report_requests
+    drop column if exists team_name
